5def1ef6a61d7f2995a5536f4e34f55ced93a1d5
Walef <walefsouza05@gmail.com>
2023-03-01 21:01:49 -0300

    Formatei a estrutura dos codigos

--- principal.php
+++ principal.php
@@ -1,49 +1,46 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
-<title>Tela principal</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="css/principal.css">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>Tela principal</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/principal.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
 </head>
 
 <body>
 
-<div class="sidenav">
-  <a href="#">Inicio<i style="padding-left: 12px;font-size: 20px;"class="fa fa-home"></i></a>
-  <a href="venda.php">Venda</a>
-  <a href="#">Estoque</a>
-  <a href="#">Cadastro</a>
-  <a href="#">Estatisticas</a>
-  <a href="#">Configurações</a>
-  <a href="#">Ajuda</a>
-  <a href="#">Sair</a>
-</div>
+  <div class="sidenav">
+    <a href="#">Inicio<i style="padding-left: 12px;font-size: 20px;" class="fa fa-home"></i></a>
+    <a href="venda.php">Venda</a>
+    <a href="#">Estoque</a>
+    <a href="#">Cadastro</a>
+    <a href="#">Estatisticas</a>
+    <a href="#">Configurações</a>
+    <a href="#">Ajuda</a>
+    <a href="#">Sair</a>
+  </div>
 
-<div class="content">
-  <h2>Tela principal</h2>
-  <p>Bem vindo.....</p>
-
- 
-  <button class="btn"><i class="fa fa-home"></i></button>
-<button class="btn"><i class="fa fa-database"></i></button>
-<button class="btn"><i class="fa fa-trash"></i></button>
-<button class="btn"><i class="fa fa-close"></i></button>
-<button class="btn"><i class="fa fa-folder"></i></button>
+  <div class="content">
+    <h2>Tela principal</h2>
+    <p>Bem vindo.....</p>
 
 
-  
-  
-</div>
+    <button class="btn"><i class="fa fa-home"></i></button>
+    <button class="btn"><i class="fa fa-database"></i></button>
+    <button class="btn"><i class="fa fa-trash"></i></button>
+    <button class="btn"><i class="fa fa-close"></i></button>
+    <button class="btn"><i class="fa fa-folder"></i></button>
+
+  </div>
 
 </body>
+
 </html>
-
-
--- venda.php
+++ venda.php
@@ -1,32 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<title>Venda</title>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="css/venda.css">
+  <title>Venda</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/venda.css">
 
 </head>
+
 <body>
 
-<div class="topnav">
-  <a href="principal.php">Inicio</a>
-  <a href="#">Ajuda</a>
-  
-</div>
+  <div class="topnav">
+    <a href="principal.php">Inicio</a>
+    <a href="#">Ajuda</a>
 
-<div class="content">
-  <h2>Vender</h2>
-  <p></p>
-  <label>Produto<label>
-  <input type="text"></input> 
-  <button type="submit">Buscar</button>
-  <button type="submit">Adicionar</button>
-</div>
+  </div>
 
-<div class="footer">
-  
-</div>
+  <div class="content">
+    <h2>Vender</h2>
+    <p></p>
+    <label>Produto<label>
+        <input type="text"></input>
+        <button type="submit">Buscar</button>
+        <button type="submit">Adicionar</button>
+  </div>
+
+  <div class="footer">
+
+  </div>
 
 </body>
+
 </html>
