47442b965bbbc6129d7f54a3f2d8e0a10ee7ae15
Walef <walefsouza05@gmail.com>
2023-03-04 19:41:20 -0300

    Trocado os nomes para português

--- login.php
+++ login.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/login.css">
 
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
 <div class="login-container">
   <section class="login" id="login">
     <header>
-      <h2>Application Name</h2>
-      <h4>Login</h4>
+      <h2>Sistema de gerenciamento</h2>
+      <h4>Conecte-se</h4>
     </header>
     <form class="login-form" action="#" method="post">
       <input type="text" class="login-input" placeholder="User" required autofocus/>
       <input type="password" class="login-input" placeholder="Password" required/>
       <div class="submit-container">
-        <button type="submit" class="login-button">SIGN IN</button>
+        <button type="submit" class="login-button">ENTRAR</button>
       </div>
     </form>
   </section>
