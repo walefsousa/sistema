7352dc25ed9d960bf234477173a11e6d63831345
Walef <walefsouza05@gmail.com>
2023-03-04 19:35:10 -0300

    Adicionei a tela de login

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+
+    <title>Document</title>
+</head>
+<body>
+<div class="login-container">
+  <section class="login" id="login">
+    <header>
+      <h2>Application Name</h2>
+      <h4>Login</h4>
+    </header>
+    <form class="login-form" action="#" method="post">
+      <input type="text" class="login-input" placeholder="User" required autofocus/>
+      <input type="password" class="login-input" placeholder="Password" required/>
+      <div class="submit-container">
+        <button type="submit" class="login-button">SIGN IN</button>
+      </div>
+    </form>
+  </section>
+</div>
+</body>
+</html>
