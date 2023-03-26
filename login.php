760bcfb32afb3a77c9ce95085621b898517889ed
Walef <walefsouza05@gmail.com>
2023-03-26 20:35:28 -0300

    Atualização do input password

--- login.php
+++ login.php
@@ -2,49 +2,49 @@
 <html lang="pt-BR">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/login.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/login.css">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <title>Login</title>
+  <title>Login</title>
 </head>
 
 <body>
-   
-<h3>LOGO</h3>
 
-<div>
-  <form action="/action_page.php">
-<<<<<<< HEAD
-  <label >Login</label>
-    <p><input type="text" name="login" placeholder="Login"></p>
+  <h3>LOGO</h3>
 
-    <label >Senha</label>
-    <p><input type="password" name="senha" placeholder="Password"></p>
-=======
-    <label>Login</label>
-    <br><input type="text" name="login" placeholder="Login"></br>
+  <div>
+    <form action="/action_page.php">
 
-    <label>Senha</label>
-    <br><input type="text" name="senha" placeholder="Password"></br>
->>>>>>> 62f85bae5b2f7f30345bb64f2bf67553d5a35bc1
+      <label>Login</label>
+      <p><input type="text" name="login" placeholder="Login"></p>
 
-    <label>Usuário</label>
-    <select id="usuario" name="usuario">
-      <option value="admin">Admin</option>
-      <option value="vendedor">Vendedor</option>
-      <option value="teste">Teste</option>
-    </select>
-  
-    <p><button type="submit" id="botao">Entrar</button></p>
+      <label>Senha</label>
+      <p><input type="password" name="senha" placeholder="Password"></p>
 
-  </form>
-</div>
+      <label>Login</label>
+      <br><input type="text" name="login" placeholder="Login"></br>
+
+      <label>Senha</label>
+      <br><input type="text" name="senha" placeholder="Password"></br>
+
+
+      <label>Usuário</label>
+      <select id="usuario" name="usuario">
+        <option value="admin">Admin</option>
+        <option value="vendedor">Vendedor</option>
+        <option value="teste">Teste</option>
+      </select>
+
+      <p><button type="submit" id="botao">Entrar</button></p>
+
+    </form>
+  </div>
 </body>
 
 </html>
