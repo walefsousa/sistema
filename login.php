ba75dd200948315e51f9f9162c24c33459d9ebfa
Walef <walefsouza05@gmail.com>
2023-04-04 11:17:43 -0300

    Ajustes no arquivo de login

--- login.php
+++ login.php
@@ -1,16 +1,25 @@
 <?php
 if (isset($_POST['user'])) {
 
-  include('conexao.php');
+    include('conexao.php');
 
-  $login = $_POST['user'];
-  $senha = $_POST['senha'];
+    $login = $_POST['user'];
+    $senha = $_POST['senha'];
 
-  $sql_code = "SELECT * FROM logins WHERE usuario = '$login' LIMIT 1";
-  $sql_exec = $mysqli->query($sql_code) or die($mysqli_connect_error);
+    $sql_code = "SELECT * FROM logins WHERE usuario = '$login' LIMIT 1";
+    $sql_exec = $mysqli->query($sql_code) or die($mysqli_connect_error);
 
-  $usuario = $sql_exec->fetch_assoc();
-  
+    $usuario = $sql_exec->fetch_assoc();
+
+    if (password_verify($senha, $usuario['senha'])) {
+
+        // echo "<h3>Bem vindo, $login</h3>";
+        header("Location: principal.php");  //redireciona o usuario para outra tela
+
+    } else {
+
+        echo "<h3 style=color:red>Usuario ou senha invalido!</h3>";
+    }
 }
 
 ?>
@@ -19,11 +28,14 @@ if (isset($_POST['user'])) {
 <html lang="pt-BR">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>login</title>
-
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="#"> 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
@@ -43,19 +55,8 @@ if (isset($_POST['user'])) {
             <button type="submit"> Entrar </button>
         </p>
 
-        <?php
-        if (password_verify($senha, $usuario['senha'])) {
-
-            echo "<h3>Bem vindo, $login</h3>";
-            header("Location: principal.php");  //redireciona o usuario para outra tela
-
-        } else {
-
-            echo "<h3>Usuario ou senha invalido!</h3>";
-            
-        } ?>
-
     </form>
+
 </body>
 
 </html>
