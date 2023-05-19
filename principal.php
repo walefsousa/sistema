23192dd0b79e9428892e12a30de903cf7a849abe
Walef <walefsouza05@gmail.com>
2023-05-19 08:45:13 -0300

    Correção no arquivo principal.php

--- principal.php
+++ principal.php
@@ -8,17 +8,13 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/principal.css">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
 </head>
 
 <body>
 
-  <div class="sidenav">
+  <div class="div-nav">
     <a href="#">Inicio<i class="fa fa-home" ></i></a>
     <a href="venda.php">Venda<i class="fa fa-shopping-cart" ></i></a>
     <a href="estoque.php">Estoque<i class="fa fa-database" ></i></a>
@@ -41,7 +37,6 @@
   
     ?>   
     <p  class="p-nome">Bem vindo <?= $string = implode(" - " , $usuario); ?></p>
-
 
   </div>
 
