97d4bc06c4a97fdf1978bfc3f680cb6bc69fd54b
Walef <walefsouza05@gmail.com>
2023-05-29 17:13:26 -0300

    Ajustes db

--- cad_produtos.php
+++ cad_produtos.php
@@ -3,19 +3,19 @@ include_once('header.php');
 include('conexao.php');
 
 
-if (isset($_POST['nome'])) {
+if (isset($_POST['codigo1'])) {
   
-    $codigo1 = $_POST['codigo'];
-    $nome1 = $_POST['nome'];
-    $preco_venda1 = $_POST['preco_venda'];
-    $preco_custo1 = $_POST['preco_custo'];
-    $estoque_min1 = $_POST['estoque_min'];
-    $estoque_atual1 = $_POST['estoque_atual'];
-    $qtd1 = $_POST['qtd'];
+    $codigo1 = $_POST['codigo1'];
+    $nome1 = $_POST['nome1'];
+    $preco_venda1 = $_POST['preco_venda1'];
+    $preco_custo1 = $_POST['preco_custo1'];
+    $estoque_min1 = $_POST['estoque_min1'];
+    $estoque_atual1 = $_POST['estoque_atual1'];
+    $qtd1 = $_POST['qtd1'];
    //$img = $_POST['images'];
 
     $sql_code = "INSERT INTO estoque(codigo, nome, preco_venda, preco_custo, estoque_minimo, estoque_atual, qtd) 
-            VALUES ('$codigo1','$nome1', '$preco_venda1', '$preco_custo1' , '$estoque_min1',' $estoque_atual1',' $qtd1')";
+            VALUES ('$codigo1','$nome1','$preco_venda1','$preco_custo1','$estoque_min1','$estoque_atual1','$qtd1')";
 
     $mysqli->query($sql_code) or die($mysqli_connect_error);
 
@@ -39,28 +39,28 @@ if (isset($_POST['nome'])) {
     <form action="" method="post">
         <div class="input-1">
             <label class="label-abas">Código/Código de barras</label>
-            <input class="cad-input" type="text" name="codigo"minlength="1" maxlength="30" required >
+            <input class="cad-input" type="text" name="codigo1"minlength="1" maxlength="30" required >
             <label class="label-abas">Descrição/Nome</label>
-            <input class="cad-input" type="text" name="nome" minlength="1" maxlength="30" required>
+            <input class="cad-input" type="text" name="nome1" minlength="1" maxlength="30" required>
           
         </div>
 
         <div class="input-2">
             <label class="label-abas">Preço de venda</label>
-            <input class="cad-input" type="text" name="preco_venda" minlength="1" maxlength="30" required>
+            <input class="cad-input" type="text" name="preco_venda1" minlength="1" maxlength="30" required>
             <label class="label-abas">Preço de Custo</label>
-            <input class="cad-input" type="text" name="preco_custo" minlength="1" maxlength="30" required>
+            <input class="cad-input" type="text" name="preco_custo1" minlength="1" maxlength="30" required>
             <label class="label-abas">Lucro %</label>
             <input class="cad-input" type="text" name="" minlength="1" maxlength="30" required>
         </div>
 
         <div class="input-3"> 
             <label class="label-abas">Estoque minimo</label>
-            <input class="cad-input" type="text" name="estoque_min" minlength="1" maxlength="30" required>
+            <input class="cad-input" type="text" name="estoque_min1" minlength="1" maxlength="30" required>
             <label class="label-abas">Estoque atual</label>
-            <input class="cad-input" type="text" name="estoque_atual"minlength="1" maxlength="30" required>
+            <input class="cad-input" type="text" name="estoque_atual1"minlength="1" maxlength="30" required>
             <label class="label-abas">Quantidade</label>
-            <input class="cad-input" type="number" name="qtd"minlength="1" maxlength="30" required>
+            <input class="cad-input" type="number" name="qtd1"minlength="1" maxlength="30" required>
         </div>
         <div class="input-4">
             <input type="file" 
